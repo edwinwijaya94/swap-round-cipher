128b420d44f964538bfdd50dc8bf015d1dbe2623
edwinwijaya94 <edwinwijaya94@yahoo.com>
2016-03-19 16:14:30 +0700

    init files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "keyGenerator.cpp"
+#include "cipher.cpp"
+
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -o exec main.cpp keyGenerator.cpp cipher.cpp -std=c++11
